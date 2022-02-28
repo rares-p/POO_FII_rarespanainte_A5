f821a0a707c72f9fa52a73ef3cd0f62ae371dc3e
rares-p <93734384+rares-p@users.noreply.github.com>
2022-02-28 19:08:10 +0200

    Add files via upload

---
+++ seminar 2/problema 2/GlobalFunctions.h
@@ -0,0 +1,5 @@
+int CompareName(Student a, Student b);
+int CompareEnglishGrade(Student a, Student b);
+int CompareMathematicsGrade(Student a, Student b);
+int CompareHistoryGrade(Student a, Student b);
+int CompareAverage(Student a, Student b);
