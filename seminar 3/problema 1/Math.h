82fa30c205a79e88e04eac5712a3e73ae73ce36a
rares-p <93734384+rares-p@users.noreply.github.com>
2022-03-07 19:20:02 +0200

    Add files via upload

---
+++ seminar 3/problema 1/Math.h
@@ -0,0 +1,14 @@
+class Math
+{
+public:
+    int Add(int,int);
+    int Add(int,int,int);
+    int Add(double,double);
+    int Add(double,double,double);
+    int Mul(int,int);
+    int Mul(int,int,int);
+    int Mul(double,double);
+    int Mul(double,double,double);
+    int Add(int count,...); // sums up a list of integers
+    char* Add(const char *, const char *);
+};
